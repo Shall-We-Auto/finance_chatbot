ab5efc3c3cb8cff12a3268615378b00180b7b8b1
junhwan26 <gjwnsghsk02@gmail.com>
2022-11-29 20:18:48 +0900

    telegram base

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import secret
 print(secret.token)
 import telegram
  
-user_id = {}
+users = {}
 bot = telegram.Bot(token=secret.token)
 updates = bot.getUpdates()
 for u in updates:
-    user_id[u.message.chat.id] = 1
-    
+    users[u.message.chat.id] = u.message.from_user.last_name
+    print(u.message)
+print(users)
 bot.sendMessage(chat_id=secret.test_id, text="Testing..")
-
 
